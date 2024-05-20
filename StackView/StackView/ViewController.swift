8cf6d3dc30a56eaf25ae353aa0695c7bed593f30
jmbae <jmbae@codegrove.co.kr>
2024-05-20 13:21:48 +0900

    Clear

--- StackView/StackView/ViewController.swift
+++ StackView/StackView/ViewController.swift
@@ -14,33 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        label.text = "Switch is OFF"
-        label.textAlignment = .center
-        
-        toggleSwitch.addAction(UIAction { [unowned self] _ in
-            print("valueChanged")
-            if self.toggleSwitch.isOn {
-                self.label.text = "Switch is ON"
-            } else {
-                self.label.text = "Switch is OFF"
-            }
-            
-        }, for: .valueChanged)
-        
-        let stackView = UIStackView(arrangedSubviews: [label, toggleSwitch])
-        stackView.axis = .vertical
-        stackView.spacing = 10
-        stackView.alignment = .center
-        
-        view.addSubview(stackView)
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-        
+
     }
 }
 
