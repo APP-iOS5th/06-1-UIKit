85d788c4ba5928d9f276fe31f9901ee541c8f3e4
jmbae <jmbae@codegrove.co.kr>
2024-05-20 09:50:13 +0900

    top,leading,trailing,bottom 제약조건 추가

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -21,8 +21,10 @@ class ViewController: UIViewController {
         label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = false
 
 //      화면 상단 상태바나 하단 홈 핸들에 겹치지 않는 영역으로 위치시킬때 사용하는 가이드라인 safeAreaLayoutGuide
-        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        label.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
 //      제약조건 하나하나 isActive 프로퍼티 설정하는 대신, activate 함수로 한번에 활성화 할 수 있다.
 //        NSLayoutConstraint.activate([
 //            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
