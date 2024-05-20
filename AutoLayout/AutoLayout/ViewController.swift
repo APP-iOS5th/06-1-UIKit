8ba8a5a30edca6a31dc400fdbfb751135f05342a
jmbae <jmbae@codegrove.co.kr>
2024-05-20 10:07:29 +0900

    최소, 중간, 최대값 제약조건

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -12,6 +12,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let box = UIView()
+        box.backgroundColor = .red
+        box.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(box)
+        
+        NSLayoutConstraint.activate([
+            box.widthAnchor.constraint(equalToConstant: 100),
+            box.heightAnchor.constraint(equalToConstant: 100),
+            
+            box.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            box.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            box.widthAnchor.constraint(lessThanOrEqualToConstant: 200),
+            box.heightAnchor.constraint(greaterThanOrEqualToConstant: 50)
+            
+        ])
     }
 }
 
