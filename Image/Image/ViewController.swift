f55f9694defe2b598f6e792d1cb4019e17c60bf6
jmbae <jmbae@codegrove.co.kr>
2024-05-20 17:33:10 +0900

    이미지와 아이콘 표시

--- Image/Image/ViewController.swift
+++ Image/Image/ViewController.swift
@@ -11,7 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let imageView = UIImageView(image: UIImage(systemName: "hare.fill"))
+        imageView.frame = CGRect(x: 50, y: 50, width: 100, height: 50)
+        imageView.tintColor = .purple
+        imageView.contentMode = .scaleAspectFit
+        self.view.addSubview(imageView)
+        
+        let flagImageView = UIImageView(image: UIImage(systemName: "flag"))
+        flagImageView.frame = CGRect(x: 50, y: 200, width: 100, height: 50)
+        flagImageView.clipsToBounds = true
+        flagImageView.contentMode = .scaleAspectFill
+        self.view.addSubview(flagImageView)
+        
     }
 
 
