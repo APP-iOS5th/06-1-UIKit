6e569b136b0c29f53e975f660804543b7414781a
jmbae <jmbae@codegrove.co.kr>
2024-05-21 14:07:52 +0900

    Year 범위 수정

--- Picker/Picker/ViewController.swift
+++ Picker/Picker/ViewController.swift
@@ -18,10 +18,12 @@ class ViewController: UIViewController {
         
         let today = Date()
         var dateComponents = DateComponents()
+        dateComponents.year = -1
+        let oneYearAgo = Calendar.current.date(byAdding: dateComponents, to: today)
         dateComponents.year = 1
         let oneYearFromNow = Calendar.current.date(byAdding: dateComponents, to: today)
         
-        datePicker.minimumDate = today
+        datePicker.minimumDate = oneYearAgo
         datePicker.maximumDate = oneYearFromNow
         
         datePicker.addAction(UIAction { [weak self] _ in
