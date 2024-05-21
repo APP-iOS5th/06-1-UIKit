fdef08fa01ba4f8580c311b0838e3df60908f635
jmbae <jmbae@codegrove.co.kr>
2024-05-21 13:23:52 +0900

    컴포넌트 추가

--- Picker/Picker/ViewController.swift
+++ Picker/Picker/ViewController.swift
@@ -15,8 +15,10 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         pickerView.translatesAutoresizingMaskIntoConstraints = false
         return pickerView
     }()
-    let options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]
     
+    let options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]
+    let options2 = ["옵션 1", "옵션 2", "옵션 3", "옵션 4", "옵션 5"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,17 +33,31 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
 
     // MARK: - UIPickerViewDataSource
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        1
+        2
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        options.count
+        switch component {
+        case 0:
+            return options.count
+        case 1:
+            return options2.count
+        default:
+            return 0
+        }
     }
     
     
     // MARK: - UIPickerViewDelegate
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return options[row]
+        switch component {
+        case 0:
+            return options[row]
+        case 1:
+            return options2[row]
+        default:
+            return ""
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
