bdc942e0ef98227b11919b66131f2eee5dded041
jmbae <jmbae@codegrove.co.kr>
2024-05-21 10:15:09 +0900

    SegmentedControl

--- SegmentedControl/SegmentedControl/ViewController.swift
+++ SegmentedControl/SegmentedControl/ViewController.swift
@@ -8,11 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let segmentedControl = UISegmentedControl(items: ["Red", "Green", "Blue"])
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.addAction(UIAction { [weak self] _ in
+            switch self?.segmentedControl.selectedSegmentIndex {
+            case 0:
+                self?.view.backgroundColor = .red
+            case 1:
+                self?.view.backgroundColor = .green
+            case 2:
+                self?.view.backgroundColor = .blue
+            default:
+                break
+            }
+        }, for: .valueChanged)
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        NSLayoutConstraint.activate([
+            segmentedControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20)
+        ])
     }
 
 
