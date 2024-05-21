3d3b9416813a1b7f70516dedf93389af8ebf9cb7
jmbae <jmbae@codegrove.co.kr>
2024-05-21 09:13:44 +0900

    UIButton

--- SegmentedControl/SegmentedControl/ViewController.swift
+++ SegmentedControl/SegmentedControl/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let button = UIButton(type: .system)
+        button.setTitle("Click me", for: .normal)
+        button.addAction(UIAction { [weak self] _ in
+            self?.count += 1
+            print("Button was tapped! \(self?.count ?? 0)")
+        }, for: .touchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
 
